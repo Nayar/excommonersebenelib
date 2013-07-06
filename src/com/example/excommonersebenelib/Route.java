f9b6f9cc3d99f1cb98e2d339dad53817263d89d0
Nayar Joolfoo <njoolfoo@gmail.com>
2013-07-07 02:39:52 +0400

    Added from and to in constructor

--- src/com/example/excommonersebenelib/Route.java
+++ src/com/example/excommonersebenelib/Route.java
@@ -6,10 +6,14 @@ import java.util.ArrayList;
 
 public class Route {
 	int no;
+	String from;
+	String to;
 	public ArrayList<BusStop> busStops;
 	
-	public Route(int x){
+	public Route(int x,String from,String to){
 		no = x;
+		this.from = from;
+		this.to = to;
 		busStops = new ArrayList<BusStop>();
 	}
 	public void addBusStop(BusStop x){
