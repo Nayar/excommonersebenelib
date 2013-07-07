9606f22ba9dc502a3bad07679aa2712163cb25fc
Nayar Joolfoo <njoolfoo@gmail.com>
2013-07-07 07:32:13 +0400

    Added constructor

--- src/com/example/excommonersebenelib/Bus.java
+++ src/com/example/excommonersebenelib/Bus.java
@@ -7,4 +7,15 @@ public class Bus {
 	Route route;
 	double x,y;
 	LatLng geoPoint;
+	
+	public Bus(String id, Route route){
+		this.id = id;
+		this.route = route;
+	}
+	
+	public void setLocation(double x,double y){
+		this.x = x;
+		this.y = y;
+		this.geoPoint = new LatLng(x, y);
+	}
 }
